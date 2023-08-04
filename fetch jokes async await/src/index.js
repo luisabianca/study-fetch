94cfcc207e575e460cf97de849dde70e8f1998ec
Bianca Santos <luisabianca93@gmail.com>
2023-08-04 00:03:17 -0300

    fix url fetch

--- fetch jokes async await/src/index.js
+++ fetch jokes async await/src/index.js
@@ -3,7 +3,7 @@ const btnNext = document.getElementById("next")
 
 const fetchJokes = async () => {
   try {
-    const cep = await fetch(`https://api.chucknorrsfsdfis.io/jokes/random`);
+    const cep = await fetch(`https://api.chucknorris.io/jokes/random`);
     const cepJSON = await cep.json();
   
     bodyHtml.innerHTML =`"${cepJSON.value}"`;
