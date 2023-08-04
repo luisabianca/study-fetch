82e16b23fedafbe2061880cf62bdbe7837647d09
Bianca Santos <luisabianca93@gmail.com>
2023-08-04 00:02:40 -0300

    format

--- fetch jokes async await/src/index.js
+++ fetch jokes async await/src/index.js
@@ -3,12 +3,13 @@ const btnNext = document.getElementById("next")
 
 const fetchJokes = async () => {
   try {
-    const cep = await fetch(`https://api.chucknorris.io/jokes/random`);
+    const cep = await fetch(`https://api.chucknorrsfsdfis.io/jokes/random`);
     const cepJSON = await cep.json();
   
     bodyHtml.innerHTML =`"${cepJSON.value}"`;
   } catch (erro) {
     bodyHtml.innerHTML = erro;
+    console.log(erro);
     btnNext.style.display = "none";
   }
 }
