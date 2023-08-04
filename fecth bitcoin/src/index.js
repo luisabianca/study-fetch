1869659202899097373ab395c6041db2a4414b89
Bianca Santos <luisabianca93@gmail.com>
2023-08-03 22:02:44 -0300

    first commit contain two little projects. Fetch cep and fetch bitcoin price

---
+++ fecth bitcoin/src/index.js
@@ -0,0 +1,17 @@
+const bodyHtml = document.getElementById("app");
+
+const fetchBitcoin = () => {
+  const bitcoin = fetch(`https://www.blockchain.com/pt/ticker`);
+
+  bitcoin
+    .then((res) => res.json())
+    .then(
+      (res) =>
+        (bodyHtml.innerText = `Preço do bitcoin: ${res.BRL.buy.toLocaleString(
+          "pt-br",
+          { style: "currency", currency: "BRL" }
+        )}`)
+    );
+};
+
+setInterval(fetchBitcoin, 200);
